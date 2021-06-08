a904a9daaa590c1366b30bfab70fd1c6ec4d089f
asafyos <asafsaf3@gmail.com>
2021-06-08 21:51:42 +0300

    Adding DB connection and Context class

---
+++ CoolBook/Data/CoolBookContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CoolBook.Models;
+
+namespace CoolBook.Data
+{
+    public class CoolBookContext : DbContext
+    {
+        public CoolBookContext (DbContextOptions<CoolBookContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CoolBook.Models.Book> Book { get; set; }
+    }
+}
