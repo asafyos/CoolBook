c92a0963fcb95090e308c9f31122767d59175a99
noyrohaker <noyrohaker98@gmail.com>
2021-06-11 12:39:45 +0300

    Added migration

--- CoolBook/Migrations/20210611091813_AddUserRole.Designer.cs
+++ CoolBook/Migrations/20210611091813_AddUserRole.Designer.cs
@@ -4,14 +4,16 @@ using CoolBook.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CoolBook.Migrations
 {
     [DbContext(typeof(CoolBookContext))]
-    partial class CoolBookContextModelSnapshot : ModelSnapshot
+    [Migration("20210611091813_AddUserRole")]
+    partial class AddUserRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
