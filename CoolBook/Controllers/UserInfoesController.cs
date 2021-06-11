79045918c1d036b65f8e963a140e4bf8b4849fc6
nivbr <niv.bruker@gmail.com>
2021-06-11 18:55:39 +0300

    User: Added UserInfo base controller and view

---
+++ CoolBook/Controllers/UserInfoesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CoolBook.Data;
+using CoolBook.Models;
+
+namespace CoolBook.Controllers
+{
+    public class UserInfoesController : Controller
+    {
+        private readonly CoolBookContext _context;
+
+        public UserInfoesController(CoolBookContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserInfoes
+        public async Task<IActionResult> Index()
+        {
+            var coolBookContext = _context.UserInfo.Include(u => u.User);
+            return View(await coolBookContext.ToListAsync());
+        }
+
+        // GET: UserInfoes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.UserInfo
+                .Include(u => u.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "UserName");
+            return View();
+        }
+
+        // POST: UserInfoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,FullName,BirthDate,Address,PhoneNumber")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userInfo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "UserName", userInfo.UserId);
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.UserInfo.FindAsync(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "UserName", userInfo.UserId);
+            return View(userInfo);
+        }
+
+        // POST: UserInfoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,FullName,BirthDate,Address,PhoneNumber")] UserInfo userInfo)
+        {
+            if (id != userInfo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userInfo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserInfoExists(userInfo.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.User, "Id", "UserName", userInfo.UserId);
+            return View(userInfo);
+        }
+
+        // GET: UserInfoes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.UserInfo
+                .Include(u => u.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(userInfo);
+        }
+
+        // POST: UserInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var userInfo = await _context.UserInfo.FindAsync(id);
+            _context.UserInfo.Remove(userInfo);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserInfoExists(int id)
+        {
+            return _context.UserInfo.Any(e => e.Id == id);
+        }
+    }
+}
