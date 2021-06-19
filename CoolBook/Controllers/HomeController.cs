7fe7ee34707497c9b3ff95f34a69e3f00e16651c
nivbr <niv.bruker@gmail.com>
2021-06-19 11:59:50 +0300

    Manager: Created a controller.

--- CoolBook/Controllers/HomeController.cs
+++ CoolBook/Controllers/HomeController.cs
@@ -28,11 +28,6 @@ namespace CoolBook.Controllers
             return View();
         }
 
-        public IActionResult Manager()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ CoolBook/Controllers/ManagerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolBook.Controllers
+{
+    public class ManagerController : Controller
+    {
+        // GET: ManagerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
