f3aeb84707c1860d2e9a7e0b7cffbecc4fbb52f7
nivbr <niv.bruker@gmail.com>
2021-06-19 16:27:08 +0300

    Manager: Added info for amount of books graph.

--- CoolBook/Controllers/ManagerController.cs
+++ CoolBook/Controllers/ManagerController.cs
@@ -29,6 +29,7 @@ namespace CoolBook.Controllers
         {
             ViewData["WatchedBooks"] = GetWatchedBooks(3);
             ViewData["BestBooks"] = GetBestBooks(3);
+            ViewData["CatAmounts"] = GetCatAmounts();
 
             return View();
         }
@@ -41,6 +42,26 @@ namespace CoolBook.Controllers
         public List<Book> GetBestBooks(int amount)
         {
             return _context.Book.OrderByDescending(b => b.Rate).Take(amount).ToList();
+        }
+
+        public class CategoryAmount
+        {
+            public Category Category { get; set; }
+            public int BookAmount { get; set; }
+        }
+
+        public List<CategoryAmount> GetCatAmounts()
+        {
+            List<CategoryAmount> result = new List<CategoryAmount>(_context.Category.Count());
+            _context.Category.Include(c => c.Books)
+                            .ToList() 
+                            .ForEach(c => result.Add(new CategoryAmount { 
+                                                            Category = c, 
+                                                            BookAmount = c.Books.Count() }));
+
+            result.Sort((ca1, ca2) => ca2.BookAmount.CompareTo(ca1.BookAmount));
+
+            return result;
         }
     }
 }
