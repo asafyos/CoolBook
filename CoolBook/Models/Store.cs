f5bb0dca316fe5747e9a861eb26f105842826427
asafyos <asafsaf3@gmail.com>
2021-06-19 15:11:59 +0300

    Adding store to DB

---
+++ CoolBook/Models/Store.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolBook.Models
+{
+    public class Store
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Lontitude { get; set; }
+        public double Latitude { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string Phone { get; set; }
+    }
+}
