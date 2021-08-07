60615057707cc7360c0948bbd744949623d5aac2
nivbr <niv.bruker@gmail.com>
2021-08-07 15:45:35 +0300

    DB: Added generation scripts for almost all tables.

---
+++ CoolBook/DB/GenerateAuthor.sql
@@ -0,0 +1,12 @@
+﻿SET IDENTITY_INSERT [dbo].[Author] ON
+INSERT INTO [dbo].[Author] ([Id], [Name], [BirthDate], [Gender], [Country]) VALUES (1, N'Niamh Bates', N'1924-01-23 00:00:00', 1, N'Malaysia')
+INSERT INTO [dbo].[Author] ([Id], [Name], [BirthDate], [Gender], [Country]) VALUES (2, N'Esha Redmond', N'1972-07-19 00:00:00', 1, N'Turkey')
+INSERT INTO [dbo].[Author] ([Id], [Name], [BirthDate], [Gender], [Country]) VALUES (3, N'Inaya Britt', N'1926-11-03 00:00:00', 1, N'Lebanon')
+INSERT INTO [dbo].[Author] ([Id], [Name], [BirthDate], [Gender], [Country]) VALUES (4, N'Masuma Knowles', N'1946-02-20 00:00:00', 0, N'Uganda')
+INSERT INTO [dbo].[Author] ([Id], [Name], [BirthDate], [Gender], [Country]) VALUES (5, N'Rohan Espinosa', N'1977-02-02 00:00:00', 0, N'Turkey')
+INSERT INTO [dbo].[Author] ([Id], [Name], [BirthDate], [Gender], [Country]) VALUES (6, N'Harlan Wallace', N'1956-01-02 00:00:00', 0, N'Great Britain')
+INSERT INTO [dbo].[Author] ([Id], [Name], [BirthDate], [Gender], [Country]) VALUES (7, N'Robert Mill', N'1988-03-03 00:00:00', 0, N'Canada')
+INSERT INTO [dbo].[Author] ([Id], [Name], [BirthDate], [Gender], [Country]) VALUES (8, N'Moshe Ashley', N'1992-11-28 00:00:00', 0, N'USA')
+INSERT INTO [dbo].[Author] ([Id], [Name], [BirthDate], [Gender], [Country]) VALUES (9, N'Jonathon Deleon', N'1985-02-06 00:00:00', 0, N'Great Britain')
+INSERT INTO [dbo].[Author] ([Id], [Name], [BirthDate], [Gender], [Country]) VALUES (10, N'Junayd Stanley', N'1971-02-24 00:00:00', 1, N'USA')
+SET IDENTITY_INSERT [dbo].[Author] OFF
