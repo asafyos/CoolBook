a4a64092343a914f75c7ebee3547c03328d95fd0
nivbr <niv.bruker@gmail.com>
2021-08-21 13:48:43 +0300

    Graphs: Changed category amount to pi chart.

--- CoolBook/wwwroot/js/graphs.js
+++ CoolBook/wwwroot/js/graphs.js
@@ -5,52 +5,54 @@ best_books_graph()
 watched_books_graph()
 
 function books_amount_graph() {
-    // set the dimensions and margins of the graph
-    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
-        width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+    var width = 960,
+        height = 500,
+        radius = Math.min(width, height) / 2;
+    var arc = d3.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(0);
 
-    // set the ranges
-    var x = d3.scaleBand()
-        .range([0, width])
-        .padding(0.1);
-    var y = d3.scaleLinear()
-        .range([height, 0]);
+    // defines wedge size
+    var pie = d3.pie()
+        .sort(null)
+        .value(function (d) { return d.count; });
 
-    // append the svg object to the right div
     var svg = d3.select("#books-amount").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+        .attr("width", width)
+        .attr("height", height)
         .append("g")
-        .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    // get the data
     d3.json("GetCatAmounts", function (data) {
-        // Scale the range of the data in the domains
-        x.domain(data.map(function (d) { return d.name; }));
-        y.domain([0, d3.max(data, function (d) { return d.count; })]);
+        // Create array of objects of search results to be used by D3
+        var parsed_data = [];
+        for (var d in data) {
+            parsed_data.push({
+                count: data[d].count,
+                name: data[d].name
+            });
+        }
+        console.log(parsed_data);
+        var color = d3.scaleOrdinal().domain([0, parsed_data.length])
+            .range(d3.schemeSet3);
+        var g = svg.selectAll(".arc")
+            .data(pie(parsed_data))
+            .enter().append("g")
+            .attr("class", "arc");
 
-        // append the rectangles for the bar chart
-        svg.selectAll(".bar")
-            .data(data)
-            .enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function (d) { return x(d.name); })
-            .attr("width", x.bandwidth())
-            .attr("y", function (d) { return y(d.count); })
-            .attr("height", function (d) { return height - y(d.count); });
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function (d) { return color(d.data.name); });
 
-        // add the x Axis
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-
-        // add the y Axis
-        svg.append("g")
-            .call(d3.axisLeft(y));
+        g.append("text")
+            .attr("transform", function (d) { return "translate(" + arc.centroid(d) + ")"; })
+            .attr("dy", ".35em")
+            .style("text-anchor", "middle")
+            .text(function (d) { return d.data.name; });
     });
 }
+
+
 
 function best_books_graph() {
     // set the dimensions and margins of the graph
