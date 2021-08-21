d5ed8e9d5713f2a052680e573f4b189549d2ef6c
guybenda <guy.bend@gmail.com>
2021-08-21 11:07:37 +0300

    Added category search

---
+++ CoolBook/wwwroot/js/Categories/Search.js
@@ -0,0 +1,21 @@
+﻿$(() => {
+    const search = getUrlParameter("search");
+    $("input#search").val(search).focus();
+})
+
+const getUrlParameter = (sParam) => {
+    let sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+
+    return false;
+};
